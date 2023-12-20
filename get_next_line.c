979d1ab878e581436a78b37add3e31b956b923a9
Hamza Talhaoui <hatalhao@e2r13p1.1337.ma>
2023-12-20 05:19:29 +0100

    Almost an update

--- get_next_line.c
+++ get_next_line.c
@@ -1,49 +1,63 @@
 #include "get_next_line.h"
 #include <string.h>
 
+char	*join_carry(char *remain)
+{
+	char	*total;
+	char	*next;
+
+	next = malloc (BUFFER_SIZE);
+	if (next == NULL)
+		return (NULL);
+	total = ft_strjoin(remain, next);
+	return (total);
+}
+
 int	look_for(char *str)
 {
 	int len;
 
 	len = 0;
-	while (str && str[len] && (str[len] != '\n'))
+	while (str)
 	{
 		if (str[len] == '\n')
+		{	
+			//printf("IN\n");
+			printf("[%d]\n", len);
 			return (len);
+		}
 		len++;
 	}
 	return (0);
 }
 
-// char	*get_split(char *str, int len)
-// {
-// 	char	*trim;
-	
-// 	trim = ft_substr(str, 0, len);
-// 	return (trim);
-// }
-
-char	*all_newl(char	*str, int size, int fd)
+char	*all_newl(char	*str, int fd)
 {
-	int 		rd;
-	int len;
+	int	rd;
+	int	len;
+	int	bool;
 
+	bool = 0;
 	len = 0;
-	str = malloc (size);
+	str = malloc (BUFFER_SIZE);
 	if (str == NULL)
 		return (NULL);
-	rd = read(fd, str, size);
+	rd = read(fd, str, BUFFER_SIZE);
 	while (str[len] && (rd > 0))
 	{
 		if (str[len] == '\n')
 		{
-			str[len] = '\0';
+			if (str[len + 1]  != '\0')
+				
+			bool = 1;
+			str[len + 1] = '\0';
 			return (str);
 		}
-		//printf("%c", str[len]);
 		len++;
 	}
-	if (rd <= 0 || size <= 0)
+	if (len == BUFFER_SIZE && bool == 0)
+		join_carry(str);
+	if (rd <= 0 || BUFFER_SIZE <= 0)
 		return (NULL);
 	return (NULL);
 }
@@ -51,21 +65,27 @@ char	*all_newl(char	*str, int size, int fd)
 char    *get_next_line(int fd)
 {
 	static char	*str;
+	char		*contain;
 	char		*tmp;
-	int			len;
 
-	tmp = all_newl(str, BUFFER_SIZE, fd);
-	len = look_for(str);
-	//printf("%s", tmp);
+	contain = NULL;
+	tmp = all_newl(contain, fd);
+	//printf(">> tmp = [%s] <<\n", tmp);
+	printf(">> contain = [%s] <<\n", contain);
+	printf(">> str = [%s] <<\n", str);
+	//free (str);
 	return (tmp);
 }
 
 int main()
 {
-	static	char *str;
+	char	*str;
+	
 	int fd = open("test.txt", O_RDWR);
 	str = get_next_line(fd);
-	printf("%s--\n",str);
+	printf("%s",str);
+	str = get_next_line(fd);
+	printf("%s",str);
 	// int i = 0;
 	// int count;
 	// printf("%p--\n", str);
