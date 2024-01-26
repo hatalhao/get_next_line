bc2dc9bca7cc3e2cc98542c8de157dc0f2a50f66
Hamza Talhaoui <hatalhao@e2r13p1.1337.ma>
2024-01-26 10:46:35 +0100

    Few more edits

--- get_next.c
+++ get_next.c
@@ -1,5 +1,7 @@
 #include "get_next_line.h"
 
+
+
 int	new_line(char *str)
 {
 	int len;
@@ -16,46 +18,49 @@ int	new_line(char *str)
 
 void    allo_read(char **str, int fd)
 {
-    long    rd;
-    char    *tmp;
+	long    rd;
+	char    *tmp;
 
-    tmp = malloc (BUFFER_SIZE + 1);
-    if (!tmp)
-        return ;
-    rd = read (fd, tmp, BUFFER_SIZE);
-    while (rd > 0)
-    {
-        *str = ft_strjoin(*str, tmp);
-        if (new_line(tmp) >= 0)
-            return ;
-        rd = read (fd, tmp, BUFFER_SIZE);
-    }
+	tmp = malloc (BUFFER_SIZE + 1);
+	if (!tmp)
+		return ;
+	rd = read (fd, tmp, BUFFER_SIZE);
+	while (rd > 0)
+	{
+		*str = ft_strjoin(*str, tmp);
+		if (new_line(tmp) >= 0)
+			return ;
+		rd = read (fd, tmp, BUFFER_SIZE);
+	}
 }
 
 char    *get_next_line(int fd)
 {
-    static char *txt;
-    char        *line;
-    char        *tmp;
+	static char *txt;
+	char        *line;
+	char        *tmp;
 
-    allo_read(&txt, fd);
-    line = ft_substr(txt , 0, new_line(txt));
-    tmp = ft_strdup(txt);
-    free(txt);
-    txt = ft_substr(txt, new_line(tmp), ft_strlen(tmp));
-    printf("%s\n", txt);
-    return (line);
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	
+	allo_read(&txt, fd);
+	printf("txt after allocation: %s\n", txt);
+	line = ft_substr(txt , 0, new_line(txt));
+	tmp = ft_strdup(txt);
+	free(txt);
+	txt = ft_substr(txt, new_line(tmp), ft_strlen(tmp));
+	printf("txt after filled from tmp: %s\n", txt);
+	return (line);
 }
 
 int main()
 {
 	char	*str;
-	//char	*str1;
 	
 	int fd = open("test.txt", O_RDWR);
 	str = get_next_line(fd);
 	printf("...%s...\n",str);
-	//str1 = get_next_line(fd);
-	//printf("%s",str1);
+	str = get_next_line(fd);
+	printf("...%s...\n",str);
 	close(fd);
 }
